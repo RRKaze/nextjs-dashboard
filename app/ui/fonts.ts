af41dcb26aa544876f8c2b17962107e8d34e4398
Xinyi Wang <xinyiwang00@gmail.com>
2024-09-15 01:15:57 -0400

    finished chapter 14

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,4 @@
-import { Inter, Lusitana } from 'next/font/google';
+import { Inter } from 'next/font/google';
  
 export const inter = Inter({ subsets: ['latin'] });
 export const lusitana = Inter({ weight: ['400', '700'], subsets: ['latin'] });
